7d8bda166b8bfdebfd4cc38596b2587f4a70eb6c
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-05-13 17:47:09 +0500

    Update NotificationFactory.java

--- src/notifications/NotificationFactory.java
+++ src/notifications/NotificationFactory.java
@@ -1,4 +1,21 @@
 package notifications;
 
 public class NotificationFactory {
+
+    public static INotification createNotification(String type) {
+
+        switch (type.toLowerCase()) {
+            case "email":
+                return new EmailNotification();
+
+            case "sms":
+                return new SMSNotification();
+
+            case "push":
+                return new PushNotification();
+
+            default:
+                return new WebNotification();
+        }
+    }
 }
