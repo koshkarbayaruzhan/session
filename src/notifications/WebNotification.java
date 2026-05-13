4a8ad60ec15a4be3044cb4a97de6ac3168da3c21
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-05-13 17:47:57 +0500

    Update WebNotification.java

--- src/notifications/WebNotification.java
+++ src/notifications/WebNotification.java
@@ -1,4 +1,19 @@
 package notifications;
 
-public class WebNotification {
+public class WebNotification implements INotification {
+
+    @Override
+    public void prepareMessage() {
+        System.out.println("Подготовка Web уведомления");
+    }
+
+    @Override
+    public void send() {
+        System.out.println("Web уведомление отправлено");
+    }
+
+    @Override
+    public String getStatus() {
+        return "Web уведомление успешно отправлено";
+    }
 }
