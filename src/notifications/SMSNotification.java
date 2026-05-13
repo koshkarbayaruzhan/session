1af983e2fbf69b7a1bb7ff8c41dea3b7b248f4c9
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-05-13 17:46:44 +0500

    Update SMSNotification.java

--- src/notifications/SMSNotification.java
+++ src/notifications/SMSNotification.java
@@ -1,4 +1,19 @@
 package notifications;
 
-public class SMSNotification {
+public class SMSNotification implements INotification {
+
+    @Override
+    public void prepareMessage() {
+        System.out.println("Подготовка SMS сообщения");
+    }
+
+    @Override
+    public void send() {
+        System.out.println("SMS отправлено");
+    }
+
+    @Override
+    public String getStatus() {
+        return "SMS успешно отправлено";
+    }
 }
