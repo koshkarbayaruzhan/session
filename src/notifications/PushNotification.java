bfc34287e69604c9c6420a39de9d70a147f7bec3
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-05-13 17:47:32 +0500

    Update PushNotification.java

--- src/notifications/PushNotification.java
+++ src/notifications/PushNotification.java
@@ -1,4 +1,19 @@
 package notifications;
 
-public class PushNotification {
+public class PushNotification implements INotification {
+
+    @Override
+    public void prepareMessage() {
+        System.out.println("Подготовка Push уведомления");
+    }
+
+    @Override
+    public void send() {
+        System.out.println("Push уведомление отправлено");
+    }
+
+    @Override
+    public String getStatus() {
+        return "Push успешно отправлен";
+    }
 }
